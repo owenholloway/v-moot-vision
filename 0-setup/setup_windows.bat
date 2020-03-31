7b0712390d2e6435e294cb6f53af4f66c7d3eb0a
Owen Holloway <owen.holloway101@gmail.com>
2020-03-31 19:48:20 +1100

    Added setup files and basic requirements

---
+++ 0-setup/setup_windows.bat
@@ -0,0 +1,2 @@
+python3.exe -m pip install --upgrade pip
+python3.exe -m pip install --user scipy opencv-python
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # v-moot-vision
+
+## Requirements
+
+- [Python 3.7.x+](https://www.python.org/downloads/)
