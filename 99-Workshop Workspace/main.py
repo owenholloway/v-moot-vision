09734b5c4a8d59c1f1c0962816ba72838e1f97d6
Owen Holloway <owen.holloway101@gmail.com>
2020-04-06 18:32:09 +1000

    Added workspace for participants

---
+++ 99-Workshop Workspace/main.py
@@ -0,0 +1,27 @@
+# this is the library we are using (opencv)
+import cv2
+
+# some debugging stuff
+print(cv2.__version__)
+print("starting vision")
+
+# open up the first (0) camera attached, if you have more than one you might
+# need to change this
+cap = cv2.VideoCapture(0)
+cap.set(3, 1080)
+cap.set(4, 720)
+
+while(True):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+
+    # do _stuff_ to the captured frame
+
+    # this is where your code will go!
+    # (this is where we build skynet ;) )
+
+    # Show the frame that we have captured
+    cv2.imshow('Captured Video', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
